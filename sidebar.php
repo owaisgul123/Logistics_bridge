a5bcd38c375f0d8ff85ea2aea154ab0377fb96b3
sufyankhan25 <kahasufyan@gmail.com>
2025-04-05 18:38:50 +0500

    Included trucker side bar for trucker

--- sidebar.php
+++ sidebar.php
@@ -4,5 +4,8 @@ $privilege = $_SESSION['privilege'];
 if ($privilege == "Admin") {    // print_r($_SESSION);
     include 'admin_sidebar.php';
 }
+if ($privilege == "Trucker") {    // print_r($_SESSION);
+    include 'trucker_sidebar.php';
+}
 
 ?>
